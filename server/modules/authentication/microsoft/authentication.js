741975b760c9a934567b73c9f210191ad3dca591
Pam Selle <pam@thewebivore.com>
2022-07-15 15:03:09 -0400

    Enable Microsoft auth
    Turn on microsoft authorization

--- server/modules/authentication/microsoft/authentication.js
+++ server/modules/authentication/microsoft/authentication.js
@@ -4,17 +4,17 @@
 // Microsoft Account
 // ------------------------------------
 
-const WindowsLiveStrategy = require('passport-microsoft').Strategy
+const MicrosoftStrategy = require('passport-microsoft').Strategy
 const _ = require('lodash')
 
 module.exports = {
   init (passport, conf) {
     passport.use(conf.key,
-      new WindowsLiveStrategy({
+      new MicrosoftStrategy({
         clientID: conf.clientId,
         clientSecret: conf.clientSecret,
         callbackURL: conf.callbackURL,
-        scope: ['User.Read', 'email', 'openid', 'profile'],
+        scope: ['user.read'],
         passReqToCallback: true
       }, async (req, accessToken, refreshToken, profile, cb) => {
         try {
